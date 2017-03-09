cb8b4b44f20095c61ea0e56a4815902b12e5ee4c
David Dataram <David Dataram>
2017-03-09 09:55:16 -0500

    Connecting to mongodb

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,12 +2,92 @@
  * Define the locations controllers 
  */
 
-module.exports.homeList = function(req, res){
-	res.render('locations-list', {title: 'Home', author : 'David'});
+module.exports.homeList = function(req, res) {
+	res.render('locations-list', {
+		title : 'Loc8r - find a place to work with wifi',
+		pageHeader : {
+			title : 'Loc8r',
+			strapline : 'Find places to work with wifi near you!'
+		},
+		sidebar : `
+		Looking for wifi and a seat? Loc8r helps you find places to work when out and about.
+         Perhaps with coffee, cake, or a pint? Let Loc8r help you find the place you're looking for.
+         `,
+		locations : [ {
+			name : 'Starcups',
+			address : '125 High Street, Reading, RG6 IPS',
+			rating : 3,
+			facilities : [ 'Hot drinks', 'Food', 'Premium wifi' ],
+			distance : '100m'
+		},
+			{
+				name : 'Starcups',
+				address : '125 High Street, Reading, RG6 IPS',
+				rating : 3,
+				facilities : [ 'Hot drinks', 'Food', 'Premium wifi' ],
+				distance : '100m'
+			},
+			{
+				name : 'Starcups',
+				address : '125 High Street, Reading, RG6 IPS',
+				rating : 3,
+				facilities : [ 'Hot drinks', 'Food', 'Premium wifi' ],
+				distance : '100m'
+			} ]
+	});
+};
+module.exports.locationInfo = function(req, res) {
+	res.render('location-info', {
+		title : 'Location Info',
+		location : {
+			name : 'Starcups',
+			address : '125 High Street, Reading, RG6 IPS',
+			rating : 3,
+			facilities : [ 'Hot drinks', 'Food', 'Premium wifi' ],
+			coords : {
+				lat: 51.455041,
+				lng: -0.9690884
+			},
+			openingTimes: [{
+				days: 'Monday - Thursday',
+				opening: '7:00am',
+				closing: '7:00pm',
+				closed: false
+			},
+			{
+				days:'Saturday',
+				opening: '8:00am',
+				closing:'5:00pm',
+				closed: false
+			},
+			{
+				days:'Sunday',
+				closed: true
+					
+			}],
+			review : [{
+				author : 'Simon Holmes',
+				timStamp : '16 July 2013',
+				content : '16 July 2013',
+				rating : 3,
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+			},
+			{
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+		},
+		 sidebar: {
+	            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+	            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+	        }
+	});
 }
-module.exports.locationInfo = function(req, res){
-	res.render('location-info', {title: 'Location Info', author: 'david'});
-}
-module.exports.addReview = function(req, res){
-	res.render('locations-review-form', {title: 'Add Review', author: 'David'});
+module.exports.addReview = function(req, res) {
+	res.render('locations-review-form', {
+		title : 'Add Review',
+		author : 'David'
+	});
 }
---
+++ app_server/models/db.js
@@ -0,0 +1,61 @@
+/**
+ *	Mongoos Database Connection model  
+ */
+
+var mongoose = require('mongoose');
+
+// Create a database URI 
+var dbUri = 'mongodb://localhost/Loc8r';
+console.log("Mode is " + process.env.NODE_ENV + ". The mongolab_uri is " + process.env.MONGOLAB_URI);
+// Change the url if working on a live server 
+if(process.env.NODE_ENV === 'production'){
+	// retrieve the database URI from the environment variable set up in th CLI s
+	dbUri = process.env.MONGOLAB_URI;
+}
+
+// Connects to the database server 
+mongoose.connect(dbUri);
+
+// Close mongoose connection, passing through an anonymous function to run when closed
+var gracefulShutdown = function(msg,callback){
+	mongoose.connection.close(function(){
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+
+// Create a group of listeners to close the connection thoroughly on all hosting servers
+
+// Listens to nodemon userss
+process.once('SIGUSR2', function(){
+	gracefulShutdown('nodemon restart', function(){
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+// Listends to application termination 
+process.on('SIGINT', function(){
+	gracefulShutdown('app termination', function(){
+		process.exit(0);
+	});
+});
+
+// Listend to Heroku server
+process.on('SIGRTERM', function(){
+	gracefulShutdown('Heroku app shutdown', function(){
+		process.exit(0);
+	});
+});
+
+//Monitor for successful connection through Mongoose 
+mongoose.connection.on('connected', function(){
+	console.log('Mongoose connected to ' + dbUri);
+});
+// Checking for connection error
+mongoose.connection.on('error', function(err){
+	console.log('Mongoose connection error ' + err);
+});
+// Checking for disconnection even
+mongoose.connection.on('disconnected', function(){
+	console.log('Mongoose disconnected');
+});
+require('./locations.js');
